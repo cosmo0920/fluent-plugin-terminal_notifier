2ceb42b27c8ffa3e46f0d23844e79b8c05de7a52
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-10-18 18:59:48 +0900

    Add more tests

--- test/plugin/test_out_terminal_notifier.rb
+++ test/plugin/test_out_terminal_notifier.rb
@@ -11,30 +11,51 @@ class TerminalNotifierOutputTest < Test::Unit::TestCase
     activate org.fluentd
   ]
 
+  EMPTY_CONFIG = %[
+  ]
+
   def create_driver(conf=CONFIG, tag='test')
     Fluent::Test::OutputTestDriver.new(Fluent::TerminalNotifierOutput, tag).configure(conf)
   end
 
-  def test_configre
-    d = create_driver
-    assert_equal "test", d.instance.title
-    assert_equal "sub_test", d.instance.sub_title
-    assert_equal "org.fluentd", d.instance.activate
+  class TestConfigure < self
+    def test_configure
+      d = create_driver
+      assert_equal "test", d.instance.title
+      assert_equal "sub_test", d.instance.sub_title
+      assert_equal "org.fluentd", d.instance.activate
+    end
+
+    def test_empty_configure
+      assert_nothing_raised do
+        d = create_driver EMPTY_CONFIG
+      end
+    end
   end
 
   def osx?
     /darwin/ =~ RUBY_PLATFORM
   end
 
-  def test_emit
-    omit("This test needs OS X.") unless osx?
+  data("all included" => [{"title" => "test title",
+                           "subtitle" => "it's sub.",
+                           "message" => "This is one of the test case"}, CONFIG],
+       "lack of sub title" => [{"title" => "test title",
+                                "message" => "This is one of the test case"},
+                               EMPTY_CONFIG],
+       "lack of sub title & title" => [{"message" => "This is one of the test case"},
+                                       EMPTY_CONFIG],
+       "all default" => [{"value" => "This is one of the test case"},
+                         EMPTY_CONFIG],
 
-    d = create_driver
+      )
+  def test_emit(data)
+    omit("This test needs OS X.") unless osx?
+    target, config = data
+    d = create_driver config
     time = Time.parse('2013-02-12 22:01:15 UTC').to_i
     d.run do
-      d.emit({"title" => "test title",
-              "subtitle" => "it's sub.",
-              "message" => "This is one of the test case"})
+      d.emit(target)
     end
 
     assert_true $?.success?
